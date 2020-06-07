c3ce91a5bf4db9161ccd822ba8644290aa9824ca
Fabiana Viana <36797672+FabiViana@users.noreply.github.com>
2020-06-07 20:36:27 -0300

    Add files via upload

---
+++ public/scripts/intro.js
@@ -0,0 +1,76 @@
+// commentes
+// document.write("Helo Word")
+
+// variaveis, tipos de dados
+
+// var myvar = "Hello Word"
+// const myconst = "Hello Word"
+
+// document.write(myvar + myconst)
+
+// string 
+// const s1 = "Isso é uma string"
+// const s2 = 'isso é uma string'
+// const s3 = `Isso é string`
+
+// document.write(s3)
+
+// number
+// const n1 = 100
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolen - true ou false
+// const btTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+
+// objeto possuem propriedades e funcionalidades
+
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     return "run Forest"
+//   }
+// }
+
+// document.write(pessoa.correr())
+
+
+// Array- Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name:"My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// funções- Funcionalidades-Atalhos-Reuso de código
+
+// function sayMyName(name){
+//   document.write(name)
+// }
+// // execultar
+// sayMyName("Douglas")
+// sayMyName("Fabiana")
+
+// condicionais
+
+// const notaFinal = 7
+// // if(true) {
+// // // caminho 1
+// // } else {
+// // // caminho 2
+// // }
+// if(notaFinal < 5){
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+// loop -repetição
+
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
+  
