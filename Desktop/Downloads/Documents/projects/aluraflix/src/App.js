3cd1a17761f92a56fca734d7e2dddfa0cd8f35ff
Fabiana Viana <fabiana.viana@reclameaqui.com.br>
2020-07-27 23:44:06 -0300

    created setup also start menu

---
+++ Desktop/Downloads/Documents/projects/aluraflix/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Menu from './components/Menu'
+
+function App() {
+  return (
+    <div>
+      <Menu/>
+      Hello Word
+    </div>
+  );
+}
+
+export default App;
---
+++ Desktop/Downloads/Documents/projects/aluraflix/src/components/Menu/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Menu() {
+  return (
+    <header>FabiVianaFlix</header>
+  )
+}
+
+export default Menu;
